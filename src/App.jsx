e2332a735f8845d64af97422f579674f1a53b244
Mohammed Afsal <mohammedafsaltut@gmail.com>
2023-12-15 19:41:52 +0530

    Add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import logo from "./assets/logo.png";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="bg-slate-900 text-slate-400 min-h-screen antialiased selection:bg-teal-300 selection:text-teal-900">
+      <a href="https://mohammedafsal.vercel.app">
+        <img src={logo} alt="" className="hidden md:block w-40 absolute right-0" />
+      </a>
       <div className="flex flex-col lg:grid lg:grid-cols-2 mx-[5%] md:mx-[7%] xl:mx-[10%] 2xl:mx-[20%]">
         <Navbar />
         <Home />
