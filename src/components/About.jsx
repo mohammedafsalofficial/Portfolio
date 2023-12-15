10563c4ba27dc9f8f11fd3ff51742c015ac4f850
Mohammed Afsal <mohammedafsaltut@gmail.com>
2023-12-15 11:02:02 +0530

    Add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+const About = () => {
+  return (
+    <section id="about" className="space-y-4 text-lg md:text-xl">
+      <h2 className="text-white text-2xl font-semibold">About</h2>
+      <p>
+        I emerged from the vibrant shores of <span>Thoothukudi, India, in 2003,</span> with a
+        spark for <span>code</span> already flickering in my soul. But it wasn't just any code;
+        it was the magic of the web, the duality of <span>front-end</span> artistry and
+        <span> back-end</span> engineering that captivated me.
+      </p>
+      <p>
+        Third-year <span>computer science </span>student and aspiring{" "}
+        <span>web developer</span> with a passion for crafting intuitive and engaging online
+        experiences. <span>Two years</span> of software development experience with a focus on
+        front-end development. Skilled in{" "}
+        <span>
+          HTML, CSS, JavaScript, React.js, and API development(Express.js, Spring Boot).{" "}
+        </span>
+        Committed to clean code, best practices, and continuous learning.
+      </p>
+    </section>
+  );
+};
+
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,11 @@
+import About from "./About";
+
 const Home = () => {
-  return <div className="h-fit md:h-screen scrollbar md:pt-24 md:pb-28"></div>;
+  return (
+    <div className="h-fit md:h-screen scrollbar md:pt-24 md:pb-28">
+      <About />
+    </div>
+  );
 };
 
 export default Home;
