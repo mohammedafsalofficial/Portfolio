88705ce6c5586d68991a54fe7920f7ca9d44de8b
Mohammed Afsal <mohammedafsaltut@gmail.com>
2023-12-18 13:26:09 +0530

    Change gradient

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="gradient-bg mouse-cursor-gradient-tracking bg-slate-900 text-slate-400 min-h-screen antialiased selection:bg-teal-300 selection:text-teal-900">
+    <div className="mouse-cursor-gradient-tracking bg-slate-900 text-slate-400 min-h-screen antialiased selection:bg-teal-300 selection:text-teal-900">
       <div className="radial"></div>
       <a href="https://mohammedafsal.vercel.app" className="hidden md:block absolute right-0">
         <img src={logo} alt="" className="w-40" />
